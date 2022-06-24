77851c55aa27d940827120c08bbf56dd38219fda
MJerich <MatthewJerich@gmail.com>
2022-06-24 16:02:33 -0700

    set up header

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+function Header() {
+  return (
+    <header className="App-header" class='top'>
+      <h1>Matthew Jerich</h1>
+      <ul id='nav'>
+        <li>
+          <span>About me</span>
+        </li>
+        <li>
+        <span>Portfolio</span>
+        </li>
+        <li>
+        <span>Contact</span>
+        </li>
+        <li>
+        <span>Resume</span>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
